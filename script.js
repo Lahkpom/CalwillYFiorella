80573ba0510804cf85df5a333207cd4328019b4a
Lahkpom <118.hidalgo.leonel.1358@gmail.com>
2025-08-18 01:24:54 -0300

    Mejoras en la imagen

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
+const contacForm = document.getElementById('contact-form');
+const name = document.getElementById('name').value;
+const email = document.getElementById('phone-number').value;
+const message = document.getElementById('message').value;
+
+contacForm.addEventListener('submit', function(e) {
     e.preventDefault();
     
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
     //Validar que el name solo sean letras y espacios y luego ponerlo todo en mayusculas
     const nameRegex = /^[A-Za-z\s]+$/;
     if (nameRegex) {
